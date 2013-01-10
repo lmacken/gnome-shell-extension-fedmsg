26f3c887022a2b0cee059baca4b6fb1695ac4c7a
Luke Macken <lmacken@redhat.com>
2013-01-10 12:32:54 -0500

    Add a script to spin up releases

---
+++ spin-extension.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+VERSION=0.1.1
+REV=1
+#rpmdev-bumpspec ~/rpmbuild/SPECS/gnome-shell-extension-fedmsg.spec
+pushd ..
+cp -R gnome-shell-extension-fedmsg{,-$VERSION}
+tar --exclude=.git -czf gnome-shell-extension-fedmsg-$VERSION.tar.gz gnome-shell-extension-fedmsg-$VERSION/
+mv *.gz ~/rpmbuild/SOURCES/
+rm -fr gnome-shell-extension-fedmsg-$VERSION
+rpmbuild -ba ~/rpmbuild/SPECS/gnome-shell-extension-fedmsg.spec
+sudo rpm -e gnome-shell-extension-fedmsg
+sudo rpm -ivh /home/lmacken/rpmbuild/RPMS/noarch/gnome-shell-extension-fedmsg-$VERSION-$REV.fc18.noarch.rpm
+popd
